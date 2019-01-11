c75e55c0a11e0235055c0e37288cd6062c9a1b8c
maxwell <mzymaxwell@163.com>
2019-01-11 22:44:02 +0800

    完后显示

---
+++ src/main/java/cn/max/control/Control.java
@@ -0,0 +1,18 @@
+package cn.max.control;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+@RestController
+public class Control {
+    @RequestMapping("/index")
+    public void hello(HttpServletRequest request, HttpServletResponse response) throws IOException {
+        System.out.println("hello");
+        response.setCharacterEncoding("utf-8");
+        response.setContentType("text/html;charset=UTF-8");
+        response.getWriter().append("欢迎进入体育购物中心");
+    }
+}
---
+++ src/main/webapp/index.jsp
@@ -0,0 +1,16 @@
+<%--
+  Created by IntelliJ IDEA.
+  User: maozhiyong
+  Date: 2018/3/24
+  Time: 20:50
+  To change this template use File | Settings | File Templates.
+--%>
+<%@ page contentType="text/html;charset=UTF-8" language="java" %>
+<html>
+<head>
+    <title>buyEveryThing</title>
+</head>
+<body>
+    <h1>welcome to my shop</h1>
+</body>
+</html>
