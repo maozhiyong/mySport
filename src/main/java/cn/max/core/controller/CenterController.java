78475659e993008bdd29eb4a3664f2f7bd05279a
maozhiyong <mzymaxwell@163.com>
2019-01-12 23:18:03 +0800

    增加注释

--- src/main/java/cn/max/core/controller/CenterController.java
+++ src/main/java/cn/max/core/controller/CenterController.java
@@ -1,15 +1,7 @@
 package cn.max.core.controller;
 
-import org.springframework.beans.propertyeditors.CustomDateEditor;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.WebDataBinder;
-import org.springframework.web.bind.annotation.InitBinder;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.support.WebBindingInitializer;
-import org.springframework.web.context.request.WebRequest;
-
-import java.text.DateFormat;
-import java.text.SimpleDateFormat;
 import java.util.Date;
 
 /**
@@ -28,11 +20,4 @@ public class CenterController{
         System.out.println(birthday);
         return "";
     }
-
-
-//    @InitBinder //implements WebBindingInitializer 这里有这个实现函数  这个函数解决的是这个control的日期转换（局部方法）
-//    public void initBinder(WebDataBinder webDataBinder, WebRequest webRequest) {
-//        DateFormat dateFormat = new SimpleDateFormat("YYYY-MM-DD HH:MM:SS");
-//        webDataBinder.registerCustomEditor(Date.class, new CustomDateEditor(dateFormat, true));
-//    }
 }
