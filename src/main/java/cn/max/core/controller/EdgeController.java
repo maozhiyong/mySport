ac6d102e091d00944a0a2a473b41a49640791436
maozhiyong <mzymaxwell@163.com>
2019-01-12 21:15:58 +0800

    remove unUseless autojump config

--- src/main/java/cn/max/core/controller/EdgeController.java
+++ src/main/java/cn/max/core/controller/EdgeController.java
@@ -2,8 +2,6 @@ package cn.max.core.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
@@ -11,16 +9,11 @@ import java.io.IOException;
 @Controller
 @RequestMapping("/")
 public class EdgeController {
-    @RequestMapping("index.shtml")
+    @RequestMapping("hello.shtml")
     public void hello(HttpServletRequest request, HttpServletResponse response) throws IOException {
         System.out.println("hello");
         response.setCharacterEncoding("utf-8");
         response.setContentType("text/html;charset=UTF-8");
         response.getWriter().append("欢迎进入体育购物中心 前台页面");
-    }
-
-    @RequestMapping(value = {" ","","/"}, method = RequestMethod.GET)
-    public void defaultJump(HttpServletRequest request, HttpServletResponse response) throws IOException {
-        response.sendRedirect(request.getContextPath()+"/index。shtml");
     }
 }
