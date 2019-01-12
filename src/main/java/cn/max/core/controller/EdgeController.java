b187c3c82c4f24a4d9d0287a0b9cfc948e147785
maozhiyong <mzymaxwell@163.com>
2019-01-12 21:15:02 +0800

    add tomcat and change controller pattern

--- src/main/java/cn/max/core/controller/EdgeController.java
+++ src/main/java/cn/max/core/controller/EdgeController.java
@@ -1,25 +1,26 @@
-package cn.max.control;
+package cn.max.core.controller;
 
+import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RestController;
+
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 
-@RestController
+@Controller
 @RequestMapping("/")
-public class Control {
-    @RequestMapping("index")
+public class EdgeController {
+    @RequestMapping("index.shtml")
     public void hello(HttpServletRequest request, HttpServletResponse response) throws IOException {
         System.out.println("hello");
         response.setCharacterEncoding("utf-8");
         response.setContentType("text/html;charset=UTF-8");
-        response.getWriter().append("欢迎进入体育购物中心");
+        response.getWriter().append("欢迎进入体育购物中心 前台页面");
     }
 
     @RequestMapping(value = {" ","","/"}, method = RequestMethod.GET)
     public void defaultJump(HttpServletRequest request, HttpServletResponse response) throws IOException {
-        response.sendRedirect(request.getContextPath()+"/index");
+        response.sendRedirect(request.getContextPath()+"/index。shtml");
     }
 }
