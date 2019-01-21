e9313f10a659e6477edfe46b2c74097eadeb691b
maozhiyong <mzymaxwell@163.com>
2019-01-21 13:21:56 +0800

    去除了图片服务器

--- src/main/java/cn/max/core/controller/uploadServer/UploadServerController.java
+++ src/main/java/cn/max/core/controller/uploadServer/UploadServerController.java
@@ -7,23 +7,23 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
 //模拟的是上传服务器 假的
-@Controller
-public class UploadServerController {
-
-	@RequestMapping(value = {"/admin/*.png"},method = RequestMethod.PUT)
-	public void putToServer(HttpServletRequest request, HttpServletResponse response) {
-		System.out.println("put");
-		response.setStatus(200);
-	}
-
-	@RequestMapping(value = {"/admin/*.png"},method = RequestMethod.POST)
-	public void postToServer(HttpServletRequest request, HttpServletResponse response) {
-		System.out.println("post");
-		response.setStatus(200);
-	}
-
-	@RequestMapping(value = {"/admin/*.png"},method = RequestMethod.GET)
-	public void getFromServer(HttpServletRequest request, HttpServletResponse response) {
-		System.out.println("get");
-	}
-}
+//@Controller
+//public class UploadServerController {
+//
+//	@RequestMapping(value = {"/admin/*.png"},method = RequestMethod.PUT)
+//	public void putToServer(HttpServletRequest request, HttpServletResponse response) {
+//		System.out.println("put");
+//		response.setStatus(200);
+//	}
+//
+//	@RequestMapping(value = {"/admin/*.png"},method = RequestMethod.POST)
+//	public void postToServer(HttpServletRequest request, HttpServletResponse response) {
+//		System.out.println("post");
+//		response.setStatus(200);
+//	}
+//
+//	@RequestMapping(value = {"/admin/*.png"},method = RequestMethod.GET)
+//	public void getFromServer(HttpServletRequest request, HttpServletResponse response) {
+//		System.out.println("get");
+//	}
+//}
